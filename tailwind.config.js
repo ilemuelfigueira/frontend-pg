6168dd1bb226ccec7e32e22c89cf725b8398d06c
ilemuelfigueira <lfigueiradev@gmail.com>
2023-09-30 23:24:49 -0300

    [controle variavel] feat: configurando prettier, tailwindcss-prettier, premierreact, postcss, tailwindcss, fonts[araboto, helvetica, helveticaneue]

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily: {
+        araboto: ['var(--font-araboto)', 'sans-serif'],
+        helvetica: ['var(--font-helvetica)', 'sans-serif'],
+        helveticaNeue: ['var(--font-helvetica-neue)', 'sans-serif'],
+      }
     },
   },
   plugins: [],
-}
+};
