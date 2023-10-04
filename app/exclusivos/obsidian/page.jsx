e75b1d1802378710359ff382a27f7ba63252e43b
ilemuelfigueira <lfigueiradev@gmail.com>
2023-10-04 04:37:10 -0300

    [Obsidian] movendo a pagina para /exclusivos/obsidian

--- app/exclusivos/obsidian/page.jsx
+++ app/exclusivos/obsidian/page.jsx
@@ -1,8 +1,8 @@
-import { YupSchemasEnum } from "@/Util/YupSchemas";
+import { YupSchemasEnum } from "@/lib/YupSchemas";
 import Product from "@/components/Product";
 
 export const metadata = {
-  title: "PG CUSTOM - Obsidian",
+  title: "Obsidian",
   description: "Controle Obsidian",
   icons: {
     icon: "/logo-32x32.png",
@@ -18,61 +18,61 @@ export default function ControleVariavel() {
         validationSchema={YupSchemasEnum.OBSIDIAN}
         shapes={[
           {
-            src: "cliente/png/ps5/branco.png",
+            src: "/cliente/png/ps5/branco.png",
             price: "00,00",
             label: "Branco",
             value: "branco",
           },
           {
-            src: "cliente/png/ps5/prata_metalico.png",
+            src: "/cliente/png/ps5/prata_metalico.png",
             price: "00,00",
             label: "Prata",
             value: "prata",
           },
           {
-            src: "cliente/png/ps5/preto.png",
+            src: "/cliente/png/ps5/preto.png",
             price: "00,00",
             label: "Preto",
             value: "preto",
           },
           {
-            src: "cliente/png/ps5/red_metalico.png",
+            src: "/cliente/png/ps5/red_metalico.png",
             price: "00,00",
             label: "Vermelho Metálico",
             value: "vermelho_metalico",
           },
           {
-            src: "cliente/png/ps5/azul_metalico.png",
+            src: "/cliente/png/ps5/azul_metalico.png",
             price: "00,00",
             label: "Azul Metálico",
             value: "azul_metalico",
           },
           {
-            src: "cliente/png/ps5/roxo.png",
+            src: "/cliente/png/ps5/roxo.png",
             price: "00,00",
             label: "Roxo",
             value: "roxo",
           },
           {
-            src: "cliente/png/ps5/rosa.png",
+            src: "/cliente/png/ps5/rosa.png",
             price: "00,00",
             label: "Rosa",
             value: "rosa",
           },
           {
-            src: "cliente/png/ps5/azul_claro.png",
+            src: "/cliente/png/ps5/azul_claro.png",
             price: "00,00",
             label: "Azul Claro",
             value: "azul_claro",
           },
           {
-            src: "cliente/png/ps5/cereja.png",
+            src: "/cliente/png/ps5/cereja.png",
             price: "00,00",
             label: "Cereja",
             value: "cereja",
           },
           {
-            src: "cliente/png/ps5/camuflado.png",
+            src: "/cliente/png/ps5/camuflado.png",
             price: "00,00",
             label: "Camuflado",
             value: "camuflado",
