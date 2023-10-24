e78541bf5bf5c3930bf19498319ac4896034539c
Marina Pereira <imarinaapereira@gmailcom>
2023-10-24 00:27:38 -0300

    [layout color] cor do layout padrao

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-import localFont from "next/font/local";
 import Navigator from "@/components/Navigator";
 import StyledComponentsRegistry from "@/lib/AntdRegistry";
 
@@ -10,63 +9,11 @@ import theme from "@/lib/AntdTheme";
 import "primeicons/primeicons.css";
 import "./globals.css";
 
-const araboto = localFont({
-  src: [
-    {
-      path: "../public/fonts/Araboto/ArabotoThin400.ttf",
-      style: "normal",
-      weight: "100",
-    },
-    {
-      path: "../public/fonts/Araboto/ArabotoNormal400.ttf",
-      style: "normal",
-      weight: "400",
-    },
-    {
-      path: "../public/fonts/Araboto/ArabotoMedium400.ttf",
-      style: "medium",
-      weight: "500",
-    },
-    {
-      path: "../public/fonts/Araboto/ArabotoLight400.ttf",
-      style: "normal",
-      weight: "300",
-    },
-    {
-      path: "../public/fonts/Araboto/ArabotoBold400.ttf",
-      style: "normal",
-      weight: "700",
-    },
-    {
-      path: "../public/fonts/Araboto/ArabotoBlack400.ttf",
-      style: "normal",
-      weight: "900",
-    },
-  ],
-  variable: "--font-araboto",
-});
-
-const helvetica = localFont({
-  src: [
-    {
-      path: "../public/fonts/Helvetica/Helvetica400.ttf",
-    },
-  ],
-  variable: "--font-helvetica",
-});
-
-const helveticaNeue = localFont({
-  src: [
-    {
-      path: "../public/fonts/HelveticaNeue/HelveticaNeueLT65Medium450.ttf",
-      style: "normal",
-      weight: "400",
-    },
-  ],
-  variable: "--font-helvetica-neue",
-});
-
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+})
 
 export const metadata = {
   title: "PG CUSTOM",
@@ -80,15 +27,15 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="pt-BR"
-      className={`${inter.variable} ${araboto.variable} ${helvetica.variable} ${helveticaNeue.variable}`}
+      className={`${poppins.variable}`}
     >
-      <body className="flex w-full flex-col items-center justify-center bg-slate-100">
+      <body className="flex w-full flex-col items-center justify-center bg-gray-200">
         <StyledComponentsRegistry>
           <ConfigProvider theme={theme}>
             <Navigator>{children}</Navigator>
           </ConfigProvider>
         </StyledComponentsRegistry>
       </body>
-    </html>
+    </html >
   );
 }
