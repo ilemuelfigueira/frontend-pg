f1daea9434814dde29ea19f376c206bdc45118f9
Marina Pereira <imarinaapereira@gmail.com>
2023-10-26 21:48:48 -0300

    [loading] utilizando icone devido a demora no carregamento

--- app/exclusivos/loading.js
+++ app/exclusivos/loading.js
@@ -1,14 +1,11 @@
 "use client"
 
-import Image from "@/components/Image";
+import { Spinner } from "@phosphor-icons/react";
 
 export default function Loading() {
   return (
     <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
-      <Image
-        src={`/logo-192x192.png`}
-        className="w-[192px] aspect-square animate-bounce"
-      />
+      <Spinner className="animate-spin" size={120} />
     </div>
   );
 }
