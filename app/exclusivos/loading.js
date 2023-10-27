9d9643744e4b9952fef91196df8f7fd64239a733
Marina Pereira <imarinaapereira@gmail.com>
2023-10-26 21:41:00 -0300

    [loading] utilizando imagem do loading estatica no sistema

--- app/exclusivos/loading.js
+++ app/exclusivos/loading.js
@@ -6,7 +6,7 @@ export default function Loading() {
   return (
     <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
       <Image
-        src={`${process.env.NEXT_PUBLIC_STORAGE_PRODUTOS}/produtos/png/logo/logo-192x192.png`}
+        src={`/logo-192x192.png`}
         className="w-[192px] aspect-square animate-bounce"
       />
     </div>
