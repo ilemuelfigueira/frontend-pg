0c247b39b44796cf4c32984d9783a9bef30204d2
Marina Pereira <imarinaapereira@gmail.com>
2023-10-26 21:13:05 -0300

    [Exclusivos] atualizando componente de loading para bounce da logo

--- app/exclusivos/loading.js
+++ app/exclusivos/loading.js
@@ -1,11 +1,14 @@
 "use client"
 
-import { CircleDashed } from "@phosphor-icons/react";
+import Image from "@/components/Image";
 
 export default function Loading() {
   return (
     <div className="top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 absolute">
-      <CircleDashed className="animate-spin text-2xl" size={150} />
+      <Image
+        src={`${process.env.NEXT_PUBLIC_STORAGE_PRODUTOS}/produtos/png/logo/logo-192x192.png`}
+        className="w-[192px] aspect-square animate-bounce"
+      />
     </div>
   );
 }
