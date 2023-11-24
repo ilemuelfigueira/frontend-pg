532acccf9f4ea252010c401ec4dfbba9767d60ba
Lemuel <lfigueiradev@gmail.com>
2023-11-24 06:49:18 -0300

    [Carrinho] adicionando mudanças na pagina de carrinho

--- app/carrinho/page.jsx
+++ app/carrinho/page.jsx
@@ -1,5 +1,5 @@
 import { Pacotes } from "@/components/Pacotes";
-import { floatToBRL } from "@/lib/util/intl";
+import { ResumoCarrinho } from "@/components/ResumoCarrinho";
 import { serverFetcher } from "@/lib/util/server-fetcher";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
@@ -32,17 +32,6 @@ async function loadData() {
   return carrinho;
 }
 
-function Resumo({ somapacotes }) {
-  return (
-    <div className="fixed bottom-0 left-1/2 flex w-screen max-w-full -translate-x-1/2 justify-between rounded-md bg-white p-4">
-      <span className="text-2xl font-bold text-slate-700">RESUMO:</span>
-      <span className="text-2xl font-bold text-slate-700">
-        {floatToBRL(parseFloat(somapacotes))}
-      </span>
-    </div>
-  );
-}
-
 export default async function Carrinho() {
   const carrinho = await loadData();
 
@@ -53,19 +42,20 @@ export default async function Carrinho() {
 
   return (
     <div className="flex w-full max-w-full flex-col gap-8">
-      <div className="flex w-full max-w-full justify-between border-b border-slate-600">
-        <span className="text-2xl font-bold text-slate-800">
+      <div className="flex w-full max-w-full justify-between border-b border-slate-600 px-4">
+        <span className="text-base md:text-2xl font-bold text-slate-800">
           <i className="pi pi-shopping-bag"></i> PRODUTO E SERVIÇO
         </span>
         <span
           data-situacao={carrinho.sgcarrinhosituacao}
-          className="text-2xl font-bold data-[situacao='PEN']:text-yellow-400"
+          className="text-base md:text-2xl font-bold data-[situacao='PEN']:text-yellow-400"
         >
           {carrinho.carrinho_situacao.nmcarrinhosituacao}
         </span>
       </div>
       <Pacotes pacotes={carrinho.pacotes} />
-      <Resumo somapacotes={somapacotes} />
+      <ResumoCarrinho somapacotes={somapacotes} />
+
     </div>
   );
 }
