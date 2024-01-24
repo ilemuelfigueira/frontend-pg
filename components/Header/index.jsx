0f17b19238ef1d033d1e494b97e32a3b8766624d
Lemuel <lfigueiradev@gmail.com>
2024-01-24 18:46:36 -0300

    [cleaning trash]

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -86,25 +86,6 @@ export function HeaderNavigator({ ...props }) {
     router.push(href);
   };
 
-  const { fcmToken } = useFcmToken();
-  // Use the token as needed
-  fcmToken && console.log("FCM token:", fcmToken);
-
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  useEffect(() => {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      const messaging = getMessaging(firebaseApp);
-      const unsubscribe = onMessage(messaging, (payload) => {
-        console.log("Foreground push notification received:", payload);
-        // Handle the received push notification while the app is in the foreground
-        // You can display a notification or update the UI based on the payload
-      });
-      return () => {
-        unsubscribe(); // Unsubscribe from the onMessage event
-      };
-    }
-  }, []);
-
   return (
     <>
       <Container {...props}>
