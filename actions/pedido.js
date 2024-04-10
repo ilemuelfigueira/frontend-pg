bae353cba51ad926864ced3a4c2d9abd21739ef4
Lemuel <lfigueiradev@gmail.com>
2024-04-10 04:15:48 -0300

    [cadastrar-pedido] feat: adicionando util de readuserorthrow no fluxo de cadastrar pedido

--- actions/pedido.js
+++ actions/pedido.js
@@ -1,33 +1,29 @@
 "use server";
 
 import { fetcher } from "@/lib/util/fetcher";
-import { createServerSupabaseClient } from "@/lib/util/supabase";
+import {
+  createServerSupabaseClient,
+  readUserOrThrow,
+} from "@/lib/util/supabase";
 
-export async function cadastrarPedido({ cdcarrinho, cdendereco}, cb) {
-  const supabase = createServerSupabaseClient();
+export async function cadastrarPedido({ cdcarrinho, cdendereco }, cb) {
+  const headers = new Headers();
+  await readUserOrThrow({
+    onOffline: () => {
+      throw new Error("Usuário não autenticado");
+    },
+    onSuccess: ({ refresh_token, access_token }) => {
+      headers.append("refresh_token", refresh_token);
+      headers.append("access_token", access_token);
+      headers.append("method", "POST");
+    },
+  });
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (!session) {
-    throw new Error("Usuário não autenticado");
-  }
-
-  const headers = new Headers()
-
-  headers.append("method", "POST")
-
-  headers.append("refresh_token", session.refresh_token)
-  headers.append("access_token", session.access_token)
-  headers.append("body", {
+  const response = await fetcher("/api/pedidos", headers, {
     cdcarrinho,
-    cdendereco
-  })
+    cdendereco,
+  });
 
-  const response = await fetcher('/pedidos', headers)
-
-  console.debug(`@/pedidos POST \n ${JSON.stringify(response)}`)
-
-  cb(response)
+  if (cb) cb(response);
+  return response;
 }
