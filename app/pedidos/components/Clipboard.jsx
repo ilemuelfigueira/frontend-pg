4c3797f05beacdd300f1afde8c4f97d84b4f84d3
Lemuel <lfigueiradev@gmail.com>
2024-04-10 04:17:41 -0300

    [clipboard] feat: componente clipboard de pedidos

---
+++ app/pedidos/components/Clipboard.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import toast from "react-hot-toast";
+
+export const Clipboard = ({ text }) => {
+  const onCopy = () => {
+    navigator.clipboard.writeText(text);
+    toast.success("texto copiado!");
+  };
+
+  return (
+    <button onClick={onCopy}>
+      <i className="pi pi-copy m-0 p-0 text-xl hover:text-slate-700"></i>
+    </button>
+  );
+};
