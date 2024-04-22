2d5ff9f9b9141e22f6efd3fc1170d6ea89b101c4
Lemuel <lfigueiradev@gmail.com>
2024-04-22 01:16:31 -0300

    [tailwind] feat: max-width theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,13 @@ module.exports = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       fontFamily: {
         poppins: ["var(--font-poppins", "sans-serif"],
+      },
+      maxWidth: {
+        "page-limit": "1346px"
       },
       colors: {
         azul_escuro: "#1B4472",
