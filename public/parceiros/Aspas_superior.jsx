4d76c513de8d7958c48d0903cf0c808ad12406e1
Lemuel <lfigueiradev@gmail.com>
2024-04-28 04:03:19 -0300

    [parceiro] feat: svgs aspas

---
+++ public/parceiros/Aspas_superior.jsx
@@ -0,0 +1,17 @@
+
+export default function AspasSuperior ({
+  className = ""
+}) {
+  return <svg className={className} width="89" height="83" viewBox="0 0 89 83" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <g clipPath="url(#clip0_122_22)">
+  <path d="M0 0.298828H37.1236V55.4529H18.5618L0 83.0001V0.298828Z" fill="#FFF"/>
+  <path d="M51 0.298828H88.1236V55.4529H69.5618L51 83.0001V0.298828Z" fill="#FFF"/>
+  </g>
+  <defs>
+  <clipPath id="clip0_122_22">
+  <rect width="89" height="83" fill="white"/>
+  </clipPath>
+  </defs>
+  </svg>
+  
+}
