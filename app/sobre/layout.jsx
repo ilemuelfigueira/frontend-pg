8260d5f478c4d89f854edce28248043209405cd5
Lemuel <lfigueiradev@gmail.com>
2024-06-18 01:39:50 -0300

    [sobre] fix: limpando

--- app/sobre/layout.jsx
+++ app/sobre/layout.jsx
@@ -1,12 +1,3 @@
-import { Breadcrumb } from "antd";
-import Link from "next/link";
-
-export const metadata = {
-  title: {
-    template: "%s | Sobre",
-  },
-};
-
 export default function Layout({ children }) {
   return (
     <div className="w-full max-w-page-limit px-1 md:px-4 lg:px-12">
