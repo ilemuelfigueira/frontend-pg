30de498f636a78a39fad0c2d7a9ba774f31edae6
Lemuel <lfigueiradev@gmail.com>
2024-06-19 19:07:14 -0300

    [pedidos] feat: acao server side para atualizar status dos pedidos

--- actions/pedido.js
+++ actions/pedido.js
@@ -2,9 +2,9 @@
 
 import { fetcher } from "@/lib/util/fetcher";
 import {
-  createServerSupabaseClient,
   readUserOrThrow,
 } from "@/lib/util/supabase";
+import { revalidateTag } from "next/cache";
 
 export async function cadastrarPedido({ cdcarrinho, cdendereco }, cb) {
   const headers = new Headers();
@@ -24,6 +24,74 @@ export async function cadastrarPedido({ cdcarrinho, cdendereco }, cb) {
     cdendereco,
   });
 
+  revalidateTag('/api/pedidos')
+
   if (cb) cb(response);
   return response;
+}
+
+export async function atualizarStatusRastreio({
+  cdpedido,
+  tracking_status,
+  tracking_code
+}, cb) {
+  const headers = new Headers();
+  await readUserOrThrow({
+    onOffline: () => {
+      throw new Error("Usuário não autenticado");
+    },
+    onSuccess: ({ refresh_token, access_token }) => {
+      headers.append("refresh_token", refresh_token);
+      headers.append("access_token", access_token);
+      headers.append("method", "PUT");
+    },
+  });
+
+  const response = await fetcher(`/api/pedidos/${cdpedido}/tracking-status`, headers, {
+    cdpedido,
+    tracking_code,
+    tracking_status
+  });
+
+  if(response?.error) {
+    console.error(`Erro ao atualizar status rastreio do pedido.`)
+    console.error(response?.error)
+  }
+
+  revalidateTag('/api/pedidos')
+
+  if (cb) cb(response)
+  return response
+}
+
+export async function atualizarStatusProducao({
+  cdpedido,
+  production_status
+}, cb) {
+  const headers = new Headers();
+  await readUserOrThrow({
+    onOffline: () => {
+      throw new Error("Usuário não autenticado");
+    },
+    onSuccess: ({ refresh_token, access_token }) => {
+      headers.append("refresh_token", refresh_token);
+      headers.append("access_token", access_token);
+      headers.append("method", "PUT");
+    },
+  });
+
+  const response = await fetcher(`/api/pedidos/${cdpedido}/production-status`, headers, {
+    cdpedido,
+    production_status
+  });
+
+  if(response?.error) {
+    console.error(`Erro ao atualizar status de produção do pedido.`)
+    console.error(response?.error)
+  }
+
+  revalidateTag('/api/pedidos')
+
+  if (cb) cb(response)
+  return response
 }
