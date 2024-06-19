c2a0d662b358c884a15423cc9fcbf0c1ec0a3bf4
Lemuel <lfigueiradev@gmail.com>
2024-06-19 01:44:17 -0300

    [pedidos] feat: enums

--- app/pedidos/enums/index.js
+++ app/pedidos/enums/index.js
@@ -3,3 +3,16 @@ export const PaymentStatus = {
   FAILED: "Falha no pagamento.",
   PENDING: "Aguardando pagamento",
 };
+
+export const ProductionStatusEnum = {
+  PENDENTE: "PENDENTE",
+  PRODUCAO: "PRODUCAO",
+  FINALIZADO: "FINALIZADO",
+};
+
+export const TrackingStatusEnum = {
+  PENDENTE: "PENDENTE",
+  POSTADO: "POSTADO",
+  ENTREGUE: "ENTREGUE",
+  "PROBLEMA NA ENTREGA": "PROBLEMA NA ENTREGA",
+};
